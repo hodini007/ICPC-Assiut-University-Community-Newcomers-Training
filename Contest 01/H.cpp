69d15b7a05ac4c135fc5ac67676fa40d65f00c7c
MD. Raiyan Bin Rafique <87417954+hodini007@users.noreply.github.com>
2024-12-03 23:52:37 +0600

    solved H.cpp
    due to precision error the code was unable to pass 3rd testcase , I just solved this and it was accepted successfully

--- Contest 01/H.cpp
+++ Contest 01/H.cpp
@@ -6,7 +6,7 @@ int main()
     double num1, num2, num3;
     cin >> num1 >> num2 >> num3;
 
-    double result = (num1 * num2) / num3;
+    double result = num1 * (num2 / num3);// to pass 3rd testcase of codeforces and it makes the calculation easier and doesn't bother due to the precision 
 
     long long myNumber = result;
     double myRes = result - myNumber;
